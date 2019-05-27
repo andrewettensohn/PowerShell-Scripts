bd6d073673833ef77ad1c1401532a135d79f45e2
Andrew Ettensohn <47993107+andrewettensohn@users.noreply.github.com>
2019-05-27 15:43:42 -0400

    Add files via upload

--- getMAClist.ps1
+++ getMAClist.ps1
@@ -1,21 +1,32 @@
+$recordCount = 0
+ 
+Write-Host "Clearing Previous Report..."
+ 
+#This sets the top line in the .txt file and deletes the old content
+$cleanReport = "Machine         MAC" | Out-File -FilePath .\MAC_list.txt
+ 
 Write-Host "Collecting MAC Address from workstations..."
  
-$recordCount = 0
+Write-Host "Report Cleared."
  
 Foreach ($storeNum in Get-Content .\stores.txt) {
  
-        $CSworkstations = ("0094", "0095", "0097")
-       
+        $CSworkstations = ("2578", "3574", "3128")
+        
+        #run script for all three machine types
         Foreach ($machineName in $CSworkstations) {
        
                 $workstation = "S" + $storeNum + $machineName
-       
+                
+                #only add record if the workstation is on the network
                 if(Test-Connection -ComputerName $workstation -Count 1 -Quiet) {
                        
                         $s = new-pssession $workstation
                        
+                        #retrieve MAC address
                         $workstationMAC = Invoke-command -Session $s -ScriptBlock {get-netadapter | SELECT MacAddress -ExpandProperty MacAddress}
                        
+                        #Add a new line to the report
                         $line = "$workstation   $workstationMAC" | Out-File -FilePath .\MAC_list.txt -append
                        
                         $recordCount++
